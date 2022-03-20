6faccaab4a3bbe9707b029ddd48a5ed6df18bd37
thamnasc <thalitanas09@gmail.com>
2022-03-20 18:39:08 -0300

    correção no tp4.3.c

--- tp4/tp4.3.c
+++ tp4/tp4.3.c
@@ -4,19 +4,30 @@
 int main() {
 
 	int vetor[TAM];
+		    /* 0    1    2    3 */
 	char abc[] = {'a', 'b', 'c', 'd'};
 	void *p;
 	int x;
 
+	/* int ocupa 4 bytes de memoria */
 	p = (void*)vetor;
 
+	/* char ocupa 1 byte cada letra, entao cabem 4 letras em um espaço de memoria do vetor inteiro */
 	for(x = 0; x < TAM * 4; x++) {
+	/* 0 % 4 = 0
+	 * 1 % 4 = 1
+	 * 2 % 4 = 2
+	 * 3 % 4 = 3
+	 * 4 % 4 = 0
+	 */
 		*(char*)p = abc[x%4];
 		p = (char*)p + 1;
 	}
 
+	/* aponta para o inicio do vetor */
 	p = (void*)vetor;
 
+	/* o valor em int em cada espaco de memoria eh o mesmo, porque foram armazenados 'abcd' em todos */
 	for(x = 0; x < TAM; x++) {
 		
 		printf("(%p) vetor[%d] = %d \n", p, x, *(int*)p);
@@ -27,7 +38,7 @@ int main() {
 
 	p = (void*)vetor;
 
-	for(x = 0; x < TAM; x++) {
+	for(x = 0; x < TAM * 4; x++) {
 		
 		printf("(%p) vetor[%d] = %c \n", p, x, *(char*)p);
 		p = (char*)p + 1;
